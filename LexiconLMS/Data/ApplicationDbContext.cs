7b0d01446bdd3348f49bde04ee23eb1427439f72
angeliaburgoss <angeliaburgos55@gmail.com>
2024-06-20 10:30:51 +0200

    updated db context

--- LexiconLMS/Data/ApplicationDbContext.cs
+++ LexiconLMS/Data/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using LexiconLMS.Models;
 
 namespace LexiconLMS.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
