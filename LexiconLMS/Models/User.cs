e55ca09e249fd41dc5193a83ec98c308346cbb2a
angeliaburgoss <angeliaburgos55@gmail.com>
2024-06-20 10:15:02 +0200

    created models mapp for entities and user entitity

---
+++ LexiconLMS/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LexiconLMS.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Course { get; set; }
+    }
+}
