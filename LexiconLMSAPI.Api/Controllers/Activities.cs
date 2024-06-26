df61e44588d192c7aec03e6d6000cfa3699b383b
BaraaAlhaib <151837010+BaraaAlhaib@users.noreply.github.com>
2024-06-26 13:44:59 +0200

    Activities Page

---
+++ LexiconLMSAPI.Api/Controllers/Activities.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LexiconLMS.Data;
+using LexiconLMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LexiconLMSAPI.Api.Controllers
+    {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+        {
+        private readonly ApplicationDbContext _context;
+
+        public ActivitiesController(ApplicationDbContext context)
+            {
+            _context = context;
+            }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Activity>>> GetActivities()
+            {
+            return await _context.Activities.ToListAsync();
+            }
+        }
+    }
