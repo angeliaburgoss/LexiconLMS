d2c707cbd52ea724b21778f72c86a8289414629c
angeliaburgoss <angeliaburgos55@gmail.com>
2024-06-28 11:03:11 +0200

    added entity to applicationuser

--- LexiconLMS/Data/ApplicationUser.cs
+++ LexiconLMS/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace LexiconLMS.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Fullname { get; set; }
     }
 
 }
